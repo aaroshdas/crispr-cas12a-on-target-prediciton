ef427bebadb65fea7142aef34a1a247e0733b4cf
Aarosh Das <aaroshdas@gmail.com>
2026-01-09 14:01:22 -0500

    proccesing data

--- run_quickr_data.py
+++ run_quickr_data.py
@@ -3,6 +3,33 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import models
 from cnn_helper import convert_to_one_hot
+from scipy.stats import spearmanr
+from matplotlib import pyplot as plt
+
+
+def plot_predictions_from_df(test_df, path):
+    temp_y_vals = []
+    temp_pred_vals = []
+    temp_x_vals = []
+    for i in range(len(test_df)):
+        temp_x_vals.append(i)
+        temp_y_vals.append(test_df.iloc[i, 1])
+        temp_pred_vals.append(test_df.iloc[i, 2])
+
+    np_y_vals = np.array(temp_y_vals)
+    np_pred_vals = np.array(temp_pred_vals)
+    rmse = np.sqrt(np.mean((np_y_vals -np_pred_vals)**2))
+    mae = np.mean(np.abs(np_y_vals - np_pred_vals))
+    rho, p_value = spearmanr(np_y_vals, np_pred_vals)
+    
+    plt.figure(figsize=(15, 5))
+    plt.plot(temp_x_vals, temp_y_vals, label='actual', linestyle='--', color='blue')
+    plt.plot(temp_x_vals, temp_pred_vals, label='preds', linestyle='-', color='red')
+    plt.ylabel(f'normalized indel freq')
+    plt.xlabel(f'RMSE {rmse: .4f} | MAE {mae: .4f} | spearman rho {rho: .4f} | p-val {p_value:.4e}')
+    plt.savefig(path)
+    print(f'RMSE {rmse: .4f} | MAE {mae: .4f} | spearman rho {rho: .4f} | p-val {p_value:.4e}')
+
 file = './datasets/raw_quickr_seqs.csv'
 
 df = pd.read_csv(file)
@@ -36,8 +63,10 @@ model = tf.keras.models.load_model(cnn_model_path)
 
 y_pred_normalized = model.predict(x_vals).squeeze()
 y_pred = (y_pred_normalized * TARGET_STD) + TARGET_MEAN
+
 df["Indel frequency Prediction"] = y_pred
+
+
 for i in y_pred:
     print(f'{i:.4f}')
-
 print(df)
